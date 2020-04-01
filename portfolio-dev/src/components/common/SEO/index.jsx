39d2a75a16e2196c153fa772632b06466cdec281
MohammadBnei <mohammadamine.banaei@gmail.com>
2020-04-02 00:05:25 +0200

    Updated to my name Updated dockerfile : Added the kernel update&upgrade + libgl

--- portfolio-dev/src/components/common/SEO/index.jsx
+++ portfolio-dev/src/components/common/SEO/index.jsx
@@ -21,7 +21,6 @@ export const SEO = ({ title = defaultTitle, description = defaultDescription, lo
 		"@type": "Organization",
 		"legalName": "${legalName}",
 		"url": "${url}",
-		"logo": "${logo}",
 		"foundingDate": "${foundingDate}",
 		"founders": [{
 			"@type": "Person",
@@ -30,22 +29,9 @@ export const SEO = ({ title = defaultTitle, description = defaultDescription, lo
 		"contactPoint": [{
 			"@type": "ContactPoint",
 			"email": "${contact.email}",
-			"telephone": "${contact.phone}",
-			"contactType": "customer service"
 		}],
-		"address": {
-			"@type": "PostalAddress",
-			"addressLocality": "${address.city}",
-			"addressRegion": "${address.region}",
-			"addressCountry": "${address.country}",
-			"postalCode": "${address.zipCode}"
-		},
 		"sameAs": [
-			"${socialLinks.twitter}",
-			"${socialLinks.google}",
-			"${socialLinks.youtube}",
 			"${socialLinks.linkedin}",
-			"${socialLinks.instagram}",
 			"${socialLinks.github}"
 		]
   	}`;
@@ -55,21 +41,16 @@ export const SEO = ({ title = defaultTitle, description = defaultDescription, lo
             <meta name="description" content={description} />
             <meta name="image" content={Thumbnail} />
 
-            <meta property="og:url" content={`${url}${location}/?ref=smakosh.com`} />
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:image" content={Thumbnail} />
-            <meta property="fb:app_id" content={social.facebook} />
 
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:creator" content={socialLinks.twitter} />
-            <meta name="twitter:site" content={social.twitter} />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image:src" content={Thumbnail} />
             <script type="application/ld+json">{structuredDataOrganization}</script>
-            <link rel="publisher" href={socialLinks.google} />
             <title>{title}</title>
             <html lang="en" dir="ltr" />
         </Helmet>
