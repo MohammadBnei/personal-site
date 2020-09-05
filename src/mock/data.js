9b54ffdeef04e751e45574d775f6bfe74e0a8529
mohammadbnei <mohammadamine.banaei@gmail.com>
2020-09-05 18:06:46 +0200

    Added personnal informations

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,94 +1,76 @@
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid'
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
-};
+    title: 'Banaei - Web Developper', // e.g: 'Name | Developer'
+    lang: 'en', // e.g: en, es, fr, jp
+    description: 'Showcasing my website, presenting my experiences and previous projects', // e.g: Welcome to my website
+}
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
-};
+    title: '',
+    name: 'Mohammad-Amine BANAEI',
+    subtitle: 'Welcome to my portfolio',
+    cta: '',
+}
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
-};
+    img: 'profile.jpg',
+    paragraphOne: 'I am a 25 years old computer science student specializing in web development.',
+    paragraphTwo: '',
+    paragraphThree: '',
+    resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+}
 
 // PROJECTS DATA
 export const projectsData = [
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-];
+    {
+        id: nanoid(),
+        img: 'project.jpg',
+        title: 'Payment API',
+        info: '',
+        info2: '',
+        url: '',
+        repo: 'https://gitlab.com/rverdeyme/projet-react-paiement',
+    },
+]
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
-};
+    cta: '',
+    btn: 'Email me!',
+    email: 'mohammadamine.banaei@gmail.com',
+}
 
 // FOOTER DATA
 export const footerData = {
-  networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'github',
-      url: '',
-    },
-  ],
-};
+    networks: [
+        {
+            id: nanoid(),
+            name: 'twitter',
+            url: '',
+        },
+        {
+            id: nanoid(),
+            name: 'codepen',
+            url: '',
+        },
+        {
+            id: nanoid(),
+            name: 'linkedin',
+            url: '',
+        },
+        {
+            id: nanoid(),
+            name: 'github',
+            url: '',
+        },
+    ],
+}
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
-};
+    isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+}
