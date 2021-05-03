d0a084f4528322ac18acc6137abaa64a2053913a
mohammadbnei <mohammadamine.banaei@gmail.com>
2021-05-03 10:32:54 +0200

    Added google tag manager (todo: delete later)

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,21 @@ module.exports = {
             },
         },
         'gatsby-plugin-offline',
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            trackingIds: [
+                process.env.GTAG_ID
+            ],
+            // tagConfig: {
+            //     optimize_id: "OPT_CONTAINER_ID",
+            //     anonymize_ip: true,
+            //     cookie_expires: 0,
+            // },
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: true,
+            },
+        }
     ],
 }
